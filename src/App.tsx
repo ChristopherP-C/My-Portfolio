5df17ba412a1c6eab3910fa4312ecb32bf55db9d
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-10-28 10:02:32 -0500

    started implementation of project card functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import MainNav from './components/mainNav';
+import { useState, useEffect } from 'react';
+import ProjectCards from './components/projectCards';
+import Projects from './pages/Projects';
+
 function App() {
+    const [cardPop, setCardPop] = useState(false);
+    const [cardContent, setCardContent] = useState('');
+
+    useEffect(() => {
+        // create if statement to check if cardContent is set to default use statre '' or if it is set to a value
+        if (cardContent !== '') {
+            setCardPop(false)
+        } else {
+            setCardPop(true);
+        };
+    }, [cardContent]);
     
   return (
     <>
@@ -8,6 +23,7 @@ function App() {
     <main>
         <Outlet />
     </main>
+    <ProjectCards cardContent={cardContent} cardPop={cardPop} />
     </>
   );
 }
