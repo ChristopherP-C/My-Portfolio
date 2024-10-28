748c2bc3987ca6a037f484da8c6a32c6c7bf1bbe
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-10-27 21:52:38 -0500

    added about me page information

---
+++ src/pages/resume.tsx
@@ -0,0 +1,7 @@
+export default function Resume() {
+    return (
+    <div>
+        <p>As I have not yet finished updating my resume, this page is not yet complete. Expect to see a completed updated resume as well as a link to download it soon</p>
+    </div>
+    );
+};
