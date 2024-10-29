c371ee16d7950b50ee0ebab7f76ef7534356a9ea
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-10-29 06:09:21 -0500

    added resume page

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function Resume() {
+
+    const embedLink = 'https://docs.google.com/document/d/e/2PACX-1vTBmWBpNKpaUjdJd1YKRZDfUSvIyNfT7PeVyJbrE-YLymciwq0dcbm2IWq8X5eNtNkppLSi3x1IlX7h/pub'
     return (
     <div>
-        <p>As I intend to recieve professional advice on how to write my ressume, this page is not yet complete. Expect to see a completed updated resume as well as a link to download it soon</p>
+        <div className='d-flex align-items-center mt-5 justify-content-center'>
+            <a className='btn btn-success' href='My Resume.pdf' download="Resume.pdf">Download my resume</a>
+        </div>
+        <br></br>
+        <div className='resume-container'>
+        <iframe src={embedLink} title='resume' width='100%' height='1000px'></iframe>  
+        </div>
     </div>
     );
 };
